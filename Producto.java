d1648af199ba10839731e81d6e64a519cd6b908c
Leon Martin <13058234+holasoymalva@users.noreply.github.com>
2023-04-07 16:11:17 -0600

    Update Producto.java

--- Producto.java
+++ Producto.java
@@ -37,3 +37,66 @@ public abstract class Producto {
 
     public abstract double calcularDescuento();
 }
+
+public class Bebida extends Producto {
+    private String tamanio;
+
+    public Bebida(String nombre, double precio, String categoria, String tamanio) {
+        super(nombre, precio, categoria);
+        this.tamanio = tamanio;
+    }
+
+    public String getTamanio() {
+        return tamanio;
+    }
+
+    public void setTamanio(String tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    @Override
+    public double calcularImpuesto() {
+        return getPrecio() * 0.16;
+    }
+
+    @Override
+    public double calcularDescuento() {
+        double descuento = 0;
+        if (getCategoria().equals("Promocion")) {
+            descuento = getPrecio() * 0.10;
+        }
+        return descuento;
+    }
+}
+
+
+public class Comida extends Producto {
+    private int calorias;
+
+    public Comida(String nombre, double precio, String categoria, int calorias) {
+        super(nombre, precio, categoria);
+        this.calorias = calorias;
+    }
+
+    public int getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(int calorias) {
+        this.calorias = calorias;
+    }
+
+    @Override
+    public double calcularImpuesto() {
+        return getPrecio() * 0.16;
+    }
+
+    @Override
+    public double calcularDescuento() {
+        double descuento = 0;
+        if (getCategoria().equals("Promocion")) {
+            descuento = getPrecio() * 0.20;
+        }
+        return descuento;
+    }
+}
